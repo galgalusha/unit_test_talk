2f52583a7fb01d9ee69de7fd37e64e6f0d2cbbb6
Sergey Petrov <sergey.petrov@kenshoo.com>
2016-12-28 16:58:01 +0200

    sergey's implementation

--- src/test/java/sync/google/Exercise_1_Matchers.java
+++ src/test/java/sync/google/Exercise_1_Matchers.java
@@ -1,12 +1,21 @@
 package sync.google;
 
 import com.google.adwords.GoogleKeywordService;
+import com.google.adwords.GoogleSelector;
+import com.google.adwords.KeywordMutateCommand;
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class Exercise_1_Matchers {
@@ -30,6 +39,7 @@ public class Exercise_1_Matchers {
 
         // TODO:
         // assert that firstKeyword has text equal to "shoes"
+        assertThat(firstKeyword, hasText("shoes"));
     }
 
     @Test
@@ -40,6 +50,7 @@ public class Exercise_1_Matchers {
 
         // TODO:
         // assert that downloaded keywords has an item with text equal to "shoes"
+        assertThat(downloadedKeywords, hasItem(hasText("shoes")));
     }
 
     @Test
@@ -50,10 +61,25 @@ public class Exercise_1_Matchers {
         // TODO:
         // verify 'googleKeywordService.getKeywords' invoked with a selector that
         // has campaign ID '1000'
+
     }
+
 
 
     private KenshooSelector someSelector() {
         return null;
     }
+
+    private Matcher<KenshooKeyword> hasText(String text) {
+
+        return new ArgumentMatcher<KenshooKeyword>() {
+
+            public boolean matches(Object o) {
+                return ((KenshooKeyword)o).text.equals(text);
+            }
+        };
+    }
+
+
+
 }
